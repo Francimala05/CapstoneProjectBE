bc64e47bb2dc44cf094abbaa3cb3e25d03f58902
Francimala05 <francimalafronte05@gmail.com>
2025-05-30 12:15:40 +0200

    Nuova APPLICATION

--- src/main/java/PizzaPazza/PizzaPazzaApplication.java
+++ src/main/java/PizzaPazza/PizzaPazzaApplication.java
@@ -1,15 +1,16 @@
 package PizzaPazza;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@Slf4j
+@EnableJpaRepositories("PizzaPazza")
+@EntityScan("PizzaPazza")
 public class PizzaPazzaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaPazzaApplication.class, args);
 	}
-
 }
+
